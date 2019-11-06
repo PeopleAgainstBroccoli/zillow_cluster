c23529b787a6ecc45acddcdf5e65067d67256d02
Garrett Williford <garrett.j.williford@gmail.com>
2019-11-06 13:51:48 -0600

    wrangle

--- acquire.py
+++ acquire.py
@@ -1,11 +1,16 @@
-import dbtools as db
 import seaborn as sns
 import matplotlib.pyplot as plt
+import env
+import MySQLdb
 
 
+def get_db_url(comm = '!', database = '!'):
+    db=MySQLdb.connect(host=env.host, user = env.user, \
+    passwd = env.password, db=database)
+    return psql.read_sql(comm, con=db)
 
-def wrangle_zillow():
-    return db.get_db_url(comm = """Select *
+
+data = db.get_db_url(comm = """Select *
 From properties_2017
 Join
 (SELECT
@@ -22,7 +27,6 @@ Group By parcelid )as sq1
 ON (sq1.parcelid=p_17.parcelid and sq1.tdate = p_17.transactiondate )) sq2
 USING (parcelid)
 WHERE (latitude IS NOT NULL AND longitude IS NOT NULL)
-AND properties_2017.propertylandusetypeid NOT IN (31, 47,246, 247, 248, 267, 290, 291)
-LIMIT 300000;""", database = 'zillow')
-
+AND properties_2017.propertylandusetypeid IN (260, 262,273, 261, 279)
+;""", database = 'zillow')
 

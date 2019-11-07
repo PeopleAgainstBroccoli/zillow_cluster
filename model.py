05d1aea6e0601ce2da144b7d36c34a9504ae14eb
Garrett Williford <garrett.j.williford@gmail.com>
2019-11-07 15:13:35 -0600

    model

--- model.py
+++ model.py
@@ -8,10 +8,18 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
 import warnings
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+warnings.filterwarnings("ignore")
+import math
+
 warnings.filterwarnings("ignore")
 
 
-def model_zillow(data):
+
+def cluster_zillow(data):
     kmeans = KMeans(n_clusters = 4)
     encoder = LabelEncoder()
     data = data.drop(columns = ['transactiondate', 'propertycountylandusecode'])
@@ -19,9 +27,27 @@ def model_zillow(data):
     train['taxdelinquencyflag'] = encoder.fit_transform(train[['taxdelinquencyflag']])
     kmeans.fit(train[['logerror', 'taxvaluedollarcnt']])
     print(kmeans.cluster_centers_)
-    sns.scatterplot(x = 'logerror', y = 'taxvaluedollarcnt', data = train, hue=kmeans.labels_, c = 'green')
-    plt.show()
+    
+    #sns.scatterplot(x = 'logerror', y = 'taxvaluedollarcnt', data = train, hue=kmeans.labels_, c = 'green')
+    #sns.scatterplot('latitude', 'longitude' , hue = kmeans.labels_, data = test)
+    print(train.info())
+    #sns.pairplot(train)
+    #plt.show()
+    return train, test
+
+
+def model_zillow(train, test):
+    model = LinearRegression()
+    x1=train.drop(columns=['logerror'])
+    x2=test.drop(columns=['logerror'])
+    y1=train[['logerror']]
+    y2=test[['logerror']]
+    model.fit(x1, y1)
+    y_pred = model.predict(y1)
+    #y_pred_proba = model.predict_proba(x2)
+    MSE = mean_squared_error(y1, y_pred)
+    print(math.sqrt(MSE))
 
 
 
-
+    

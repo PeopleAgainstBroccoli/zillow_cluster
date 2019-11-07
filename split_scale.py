8b87af1cf08dadca8d87d6bf0ab14813c489727f
davidtenorio1 <dtenorio2169@gmail.com>
2019-11-07 16:16:53 -0600

    scaler and split functions added. more exploring done.

--- split_scale.py
+++ split_scale.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 import seaborn as sns
-from sklearn.preprocessing import StandardScaler, QuantileTransformer, PowerTransformer, RobustScaler, MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.model_selection import train_test_split
 
 
@@ -15,6 +15,14 @@ def standard_scaler(X):
     scaler = StandardScaler(copy=True, with_mean=True, with_std=True).fit(X)
     scaled_X = pd.DataFrame(scaler.transform(X),columns=X.columns.values).set_index([X.index.values])
     return scaled_X
+
+
+def min_max_scaler(X):
+    train, test = split_my_data(X)
+    scaler = MinMaxScaler(copy=True, feature_range=(0,1)).fit(train)
+    train = pd.DataFrame(scaler.transform(train), columns=train.columns.values).set_index([train.index.values])
+    test =  pd.DataFrame(scaler.transform(test), columns=test.columns.values).set_index([test.index.values])
+    return train, test, scaler
 
 
 

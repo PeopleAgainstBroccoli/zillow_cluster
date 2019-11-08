8f0263a7b6ee8e16096464c016bdfb47ad84e6ad
Garrett Williford <garrett.j.williford@gmail.com>
2019-11-08 14:27:37 -0600

    model

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-import dbtools as db
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,15 +8,17 @@ from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
 import warnings
 from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import MinMaxScaler
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeRegressor
 warnings.filterwarnings("ignore")
 import math
-
-
 
 
 def results_train(logit, y_pred, y_pred_proba, x_train, y_train):
@@ -52,10 +53,12 @@ def results_train(logit, y_pred, y_pred_proba, x_train, y_train):
 
 
 def return_xy(train, test):
-    x1 = train[['bedroomcnt', 'poolcnt', 'taxvaluedollarcnt', 'calculatedfinishedsquarefeet', 'bathroomcnt', \
-                'taxdelinquencyflag', 'taxdelinquencyyear']]
-    x2 = test[['bedroomcnt', 'poolcnt', 'taxvaluedollarcnt', 'calculatedfinishedsquarefeet', 'bathroomcnt', \
-                'taxdelinquencyflag', 'taxdelinquencyyear']]
+    #x1 = train[['bedroomcnt', 'poolcnt', 'taxvaluedollarcnt', 'calculatedfinishedsquarefeet', 'bathroomcnt', \
+     #           'taxdelinquencyflag', 'taxdelinquencyyear']]
+    #x2 = test[['bedroomcnt', 'poolcnt', 'taxvaluedollarcnt', 'calculatedfinishedsquarefeet', 'bathroomcnt', \
+     #           'taxdelinquencyflag', 'taxdelinquencyyear']]
+    x1 = train[['bedroomcnt', 'bathroomcnt', 'calculatedfinishedsquarefeet', 'taxdelinquencyflag']]
+    x2 = test[['bedroomcnt', 'bathroomcnt', 'calculatedfinishedsquarefeet', 'taxdelinquencyflag']]
     y1=train[['logerror']]
     y2=test[['logerror']]
     return x1, x2, y1, y2
@@ -71,23 +74,10 @@ def cluster_zillow(data):
     kmeans.fit(train[['taxvaluedollarcnt']])
     scaler.fit_transform(train)
     train['cluster_group'] = kmeans.labels_
-    print(train)
     print('AVERAGE LOG ERROR BY CLUSTER \n%s' % (train.groupby(kmeans.labels_)['logerror'].mean()))
     train['cluster'] = kmeans.labels_
-    print(train)
-    #g = sns.FacetGrid(train, col = 'cluster')
-    #g = g.map(plt.scatter, 'latitude', 'longitude', alpha = .5)
     sns.scatterplot('latitude', 'longitude', data = train, hue=kmeans.labels_, c = 'green')
-    
-    plt.show()
     return train, test
-
-
-
-#sns.countplot(x = 'logerror', data = train, hue = train.groupby(kmeans.labels_)['logerror'].mean())
-#sns.scatterplot('latitude', 'longitude' , hue = kmeans.labels_, data = train)
-#print(train.info())
-
 
 def baseline_model_zillow(train, test):
     model = LinearRegression()
@@ -99,21 +89,7 @@ def baseline_model_zillow(train, test):
     y_pred = model.predict(x1)
     #y_pred_proba = model.predict_proba(x2)
     MSE = mean_squared_error(y1, y_pred)
-    print(math.sqrt(MSE))
-
-
-
-def model_zillow_logistic(train, test):
-    x1=train.drop(columns=['logerror'])
-    x2=test.drop(columns=['logerror'])
-    y1=train[['logerror']]
-    y2=test[['logerror']]
-    logit = LogisticRegression(C=1, class_weight={1:2}, random_state = 123, solver='saga').fit(x1, y1)
-    y_pred = logit.predict(x1)
-    y_pred_proba = logit.predict_proba(x1)
-    print(y_pred)
-
-
+    return math.sqrt(MSE)
 
 
 def model_zillow_linear(train, test):
@@ -122,7 +98,7 @@ def model_zillow_linear(train, test):
     model.fit(x1, y1)
     y_pred = model.predict(x1)
     MSE = mean_squared_error(y1, y_pred)
-    print(math.sqrt(MSE))
+    return math.sqrt(MSE)
 
 
 def model_zillow_tree(train, test):
@@ -130,8 +106,15 @@ def model_zillow_tree(train, test):
     tree = DecisionTreeRegressor(max_depth = 6, random_state = 123)
     tree.fit(x1, y1)
     y_pred = tree.predict(x1)
-    #y_pred_proba = tree.predict_proba(x1)
-    results_train(tree, y_pred, '!', x1, y1)
+    MSE = mean_squared_error(y1, y_pred)
+    return math.sqrt(MSE)
     
 
+
+def model_zillow_forest(train, test):
+    x1, x2, y1, y2 = return_xy(train, test)
+    forest = RandomForestRegressor(max_depth = 10, random_state = 123).fit(x1, y1)
+    y_pred = forest.predict(x1)
+    MSE = mean_squared_error(y1, y_pred)
+    return math.sqrt(MSE)
 
